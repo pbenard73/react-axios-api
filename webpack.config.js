49a1049f5462237afad77e97e1676886a3e10c96
BENARD Patrick <pbenard73@gmail.com>
2021-09-12 17:44:09 +0200

    start architecture

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path')
+
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const nodeExternals = require('webpack-node-externals');
+
+module.exports = {
+    mode:'production',
+  entry: {
+    index: './lib/index.js',
+  },
+  externals: [nodeExternals()],
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist'),
+    library: 'react-axios-api',
+    libraryTarget: 'commonjs-module'
+  },
+  plugins: [new CleanWebpackPlugin()],
+  module: {
+    rules: [
+     {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
+      { 
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+        include: path.resolve(__dirname, './src')
+      }
+    ]
+  }
+}
