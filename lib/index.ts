6b69683b550b347a8e423ed3afd6cc3ece3762de
BENARD Patrick <pbenard73@gmail.com>
2024-01-28 11:48:18 +0100

    back to avoid axios bug in copil

--- lib/index.ts
+++ lib/index.ts
@@ -1,60 +1,64 @@
 import { useState, useEffect } from "react";
-import {mergeDeepLeft} from 'ramda'
+import { mergeDeepLeft } from "ramda";
 
 import Api from "./Api";
 
 // declare module 'react-axios-api';
 
 export interface ApiPoolItem {
-  path: string
-  method?: string
-  domain?: string
-  body?: any
-  headers?: any
-  options?: any
+  path: string;
+  method?: string;
+  domain?: string;
+  body?: any;
+  headers?: any;
+  options?: any;
 }
 export interface ApiPool {
-  [routeName: string]: ApiPoolItem
+  [routeName: string]: ApiPoolItem;
 }
 
 export interface HookItem {
-  isCalling: boolean
-  data: any | null
-  error: any | null
+  isCalling: boolean;
+  data: any | null;
+  error: any | null;
 }
 
 export interface ExtraOptions {
-  parseData?: (data:any) => any
-  [name: string]: any
+  parseData?: (data: any) => any;
+  [name: string]: any;
 }
 
 export interface ApiItem {
-  (options?: any, body?: any, givenExtraOptions?: ExtraOptions): Promise<any>
-  url(options?: any): string
-  useHook(options?: any, body?: any, givenExtraOptions?: any): HookItem
+  (options?: any, body?: any, givenExtraOptions?: ExtraOptions): Promise<any>;
+  url(options?: any): string;
+  useHook(options?: any, body?: any, givenExtraOptions?: any): HookItem;
 }
 
 export interface GeneratedApi {
-  [routeName: string]: ApiItem
+  [routeName: string]: ApiItem;
 }
 
-export type Prefix = string | (() => string)
+export type Prefix = string | (() => string);
 
 const makeApiFx = (apiPool: ApiPool = {}, prefix: Prefix = "", extraOptions: ExtraOptions = {}): GeneratedApi => {
   let data: GeneratedApi = {};
 
-  const cleanRoute = (route: any) => ({ ...route, path: (typeof prefix === 'string' ? prefix : prefix()) + route.path })
+  const cleanRoute = (route: any) => ({
+    ...route,
+    path: (typeof prefix === "string" ? prefix : prefix()) + route.path,
+  });
 
   Object.keys(apiPool).forEach((routeName) => {
     const route = { ...apiPool[routeName] };
 
-    const mergedOptions = (givenOptions = {}) => mergeDeepLeft(extraOptions, givenOptions)
+    const mergedOptions = (givenOptions = {}) => mergeDeepLeft(extraOptions, givenOptions);
 
-    const callFunction = (options?: any, body?: any, givenExtraOptions?: ExtraOptions) => Api.call(cleanRoute(route), options, body, mergedOptions(givenExtraOptions));
+    const CallFunction = (options?: any, body?: any, givenExtraOptions?: ExtraOptions) =>
+      Api.call(cleanRoute(route), options, body, mergedOptions(givenExtraOptions));
 
-    callFunction.url = (options?: any) => Api.url(cleanRoute(route), options);
+    CallFunction.url = (options?: any) => Api.url(cleanRoute(route), options);
 
-    callFunction.useHook = (options?: any, body?: any, givenExtraOptions?: any) => {
+    CallFunction.useHook = (options?: any, body?: any, givenExtraOptions?: any) => {
       const [calling, setCalling] = useState(true);
       const [success, setSuccess] = useState();
       const [error, setError] = useState();
@@ -69,7 +73,7 @@ const makeApiFx = (apiPool: ApiPool = {}, prefix: Prefix = "", extraOptions: Ext
       return { isCalling: calling, data: success, error };
     };
 
-    data[routeName] = callFunction;
+    data[routeName] = CallFunction;
   });
 
   return data;
